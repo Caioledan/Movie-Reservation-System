6d428b6f78bc8cfc839b1fb793946d5e3cf6c57f
Caio Lemos <caioledan@gmail.com>
2026-09-02 17:07:26 -0300

    feat: github actions and stripe tests

--- src/payment/payment.controller.spec.ts
+++ src/payment/payment.controller.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentController } from './payment.controller';
+import { StripeService } from './stripe/stripe.service';
+import { TicketService } from 'src/ticket/ticket.service';
 
 describe('PaymentController', () => {
   let controller: PaymentController;
 
   beforeEach(async () => {
+    const mockStripeService = {
+      createCheckoutSession: jest.fn(),
+      constructEventFromPayload: jest.fn(),
+    };
+
+    const mockTicketService = {
+      updateTicket: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PaymentController],
+      providers: [
+        { provide: StripeService, useValue: mockStripeService },
+        { provide: TicketService, useValue: mockTicketService },
+      ],
     }).compile();
 
     controller = module.get<PaymentController>(PaymentController);
--- src/payment/stripe/stripe.service.spec.ts
+++ src/payment/stripe/stripe.service.spec.ts
@@ -5,12 +5,22 @@ describe('StripeService', () => {
   let service: StripeService;
 
   beforeEach(async () => {
+    process.env.STRIPE_API_SECRET = 'sk_test_mock';
+    process.env.STRIPE_WEBHOOK_SECRET = 'whsec_mock';
+    process.env.FRONTEND_URL = 'http://localhost:3000';
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [StripeService],
     }).compile();
 
     service = module.get<StripeService>(StripeService);
   });
+
+  afterEach(() => {
+    delete process.env.STRIPE_API_SECRET;
+    delete process.env.STRIPE_WEBHOOK_SECRET;
+    delete process.env.FRONTEND_URL;
+  })
 
   it('should be defined', () => {
     expect(service).toBeDefined();
